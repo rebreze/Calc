c92d25bae487cd9ca39a66ebd85c06abb44df73b
bealorf <minecrafthipexl@gmail.com>
2024-10-10 16:06:40 +0300

    АВАВА V2.0

--- Program.cs
+++ Program.cs
@@ -10,11 +10,21 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            int a, b, resultat;
+            int a, b, resultat,dey;
             Console.WriteLine("Введите первое число:");
             a= Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите второе число:");
             b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("сложение(1) или вычитание(2): ");
+            dey = Convert.ToInt32(Console.ReadLine());
+            if (dey == 2)
+                {
+                resultat = a - b;
+                Console.WriteLine("Результат: " + resultat);
+                Console.ReadKey();
+
+            }
+            else
             resultat = a+b;
             Console.WriteLine("Результат: "+ resultat);
             Console.ReadKey();
